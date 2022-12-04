808217c069edd5c808ad0bb98bf66162fb6fa45d
rofinoorsalim <58476731+rofinoorsalim@users.noreply.github.com>
2022-12-04 12:00:44 +0700

    Update sprite HD

--- BengkelGizi/Assets/Scripts/Foodpedia.cs
+++ BengkelGizi/Assets/Scripts/Foodpedia.cs
@@ -82,23 +82,23 @@ public class Foodpedia : MonoBehaviour
     {
         if (Nutrition == Protein)
         {
-            setInfo("Ikan Tuna", ProteinDefault, "");
+            setInfo("Ikan Tuna", ProteinDefault, "Tuna merupakan ikan air laut yang berasal dari bangsa Thunnini. Daging ikan tuna memiliki warna merah muda sampai merah tua karena ototnya yang memiliki banyak myoglobin. Per 100 g BDD, tuna memiliki kandungan protein sebesar 28 g.");
         }
         if (Nutrition == Karbohidrat)
         {
-            setInfo("Roti", KarbohidratDefault, "Bread");
+            setInfo("Roti", KarbohidratDefault, "Roti merupakan makanan yang dibuat dengan bahan dasar utama tepung terigu dan air. Per 100 g BDD, roti memiliki kandungan karbohidrat sebesar 49 g.");
         }
         if (Nutrition == Serat)
         {
-            setInfo("Buncis", SeratDefault, "Apple");
+            setInfo("Buncis", SeratDefault, "Buncis adalah tanaman kacang-kacangan yang dipercayai berasal dari Amerika Tengah dan Amerika Selatan. Per 100 g, buncis memiliki kandungan serat sebesar 3.4 g");
         }
         if (Nutrition == VitaminC)
         {
-            setInfo("Jambu Biji", VitaminCDefault, "Avocado");
+            setInfo("Jambu Biji", VitaminCDefault, "Jamu biji adalah buah tropis yang berasal dari Amerika Tengah dan Amerika Selatan. Per 100 g, jambu biji memiliki kandungan Vitamin C sebesar 228,3 g.");
         }
         if (Nutrition == Kalsium)
         {
-            setInfo("Keju", KalsiumDefault, "Almond");
+            setInfo("Keju", KalsiumDefault, "Keju merupakan salah satu produk olahan dari susu, biasanya susu sapi, kerbau, kambing, atau domba, melalui proses pengentalan atau koagulasi. Keju memiliki kandungan kalsium sebesar 721 mg.");
         }
     }
 }
